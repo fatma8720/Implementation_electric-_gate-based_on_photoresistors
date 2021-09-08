5ed22fed1938dbace98843124a9fe32e5af8ff9f
Fatma Elzahraa Atef <fatmaatef8720@gmail.com>
2021-09-08 22:23:16 +0200

    Impelementation_E_Gate.ino

---
+++ Impelementation_E_Gate/Impelementation_E_Gate.ino
@@ -0,0 +1,48 @@
+#define LDR_1 A0
+#define LDR_2 A1
+const int LDR_Min=950;
+const int buzzer = 9; 
+bool LDR1=LOW;
+bool LDR2=LOW;
+
+int PeopleNumber=0;
+
+void setup() {
+  Serial.begin(9600);
+   pinMode(buzzer, OUTPUT); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(analogRead(LDR_1)<LDR_Min && analogRead(LDR_2)>LDR_Min && LDR1==LOW && LDR2==LOW)
+    LDR1=HIGH;
+  if(analogRead(LDR_1)>LDR_Min && analogRead(LDR_2)<LDR_Min && LDR1==HIGH && LDR2==LOW)
+   {
+      while(analogRead(LDR_2)<LDR_Min)
+      { } 
+       PeopleNumber++;
+       Serial.println("People in room: " + (String)PeopleNumber);
+       LDR1=LOW;
+       LDR2=LOW;
+   }
+  
+  if(analogRead(LDR_1)>LDR_Min && analogRead(LDR_2)<LDR_Min && LDR1==LOW && LDR2==LOW)
+    LDR2=HIGH;
+
+  if(analogRead(LDR_1)<LDR_Min && analogRead(LDR_2)>LDR_Min && LDR1==LOW && LDR2==HIGH)
+   {
+      while(analogRead(LDR_1)<LDR_Min)
+      { }
+      PeopleNumber=PeopleNumber>0?PeopleNumber-1:0;
+      Serial.println("People in room: " + (String)PeopleNumber);
+      LDR2=LOW;
+      LDR1=LOW;
+   }
+
+   if (PeopleNumber==5){
+  tone(buzzer, 1000); 
+  delay(1000);        
+  noTone(buzzer);     
+  delay(1000);      
+   }
+}
